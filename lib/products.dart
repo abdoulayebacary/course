6b6ba28f1d586557e5704b4b05c01afa612d79ab
abdoulaye bacary <abdoulayebacary@gmail.com>
2019-09-09 13:15:38 +0000

    conditional view

--- lib/products.dart
+++ lib/products.dart
@@ -18,13 +18,19 @@ class Products extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return products.length > 0
-        ? ListView.builder(
-            itemBuilder: _buildProductItem,
-            itemCount: products.length,
-          )
-        : Center(
-            child: Text('Pas de produits, SVP ajouter en quelque'),
-          );
+    Widget productCard;
+
+    if (products.length > 0) {
+      productCard = ListView.builder(
+        itemBuilder: _buildProductItem,
+        itemCount: products.length,
+      );
+    } else {
+      productCard = Center(
+        child: Text('Pas de produits, SVP ajouter en quelque'),
+      );
+    }
+
+    return productCard;
   }
 }
