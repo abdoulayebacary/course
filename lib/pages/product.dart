0e6a791e654a9ff0a25cd9e21ba18998c8b71c8c
abdoulaye bacary <abdoulayebacary@gmail.com>
2019-09-09 15:23:54 +0000

    adding Reaction upon Button Pressed

--- lib/pages/product.dart
+++ lib/pages/product.dart
@@ -1,39 +1,44 @@
 import 'package:flutter/material.dart';
-
+import 'dart:async';
 class ProductPage extends StatelessWidget {
   final String title, imageUrl;
 
-
-ProductPage({@required this.title, @required this.imageUrl});
+  ProductPage({@required this.title, @required this.imageUrl});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: Text(title),
-        // backgroundColor: Colors.red,
-      ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: <Widget>[
-          Image.asset(imageUrl),
-          Container(
-            padding: EdgeInsets.all(10.0),
-            child: Text(title),
-          ),
-          Container(
-            padding: EdgeInsets.all(10.0),
-            child: RaisedButton(
-              color: Theme.of(context).primaryColor,
-              child: Text('DELETE'),
-               onPressed: () => Navigator.pop(context, true),
+    return WillPopScope(
+      onWillPop: () {
+        print('back button');
+        Navigator.pop(context,false);
+        return Future.value(false);
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text(title),
+          // backgroundColor: Colors.red,
+        ),
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Image.asset(imageUrl),
+            Container(
+              padding: EdgeInsets.all(10.0),
+              child: Text(title),
             ),
-          ),
-         
-        ],
+            Container(
+              padding: EdgeInsets.all(10.0),
+              child: RaisedButton(
+                color: Theme.of(context).primaryColor,
+                child: Text('DELETE'),
+                onPressed: () => Navigator.pop(context, true),
+              ),
+            ),
+          ],
+        ),
+        // margin: EdgeInsets.all(0.0),
       ),
-      // margin: EdgeInsets.all(0.0),
     );
   }
 }
